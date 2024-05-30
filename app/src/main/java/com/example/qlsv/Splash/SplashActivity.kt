205390ce17b9df637641b222bdafd42896c0d365
nbhieu1404 <nbhieu140402@gmail.com>
2024-05-30 09:51:20 +0700

    new

---
+++ app/src/main/java/com/example/qlsv/Splash/SplashActivity.kt
@@ -0,0 +1,40 @@
+package com.example.qlsv.Splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.qlsv.Account.LoginActivity
+import com.example.qlsv.MainActivity
+import com.example.qlsv.R
+import com.example.qlsv.databinding.ActivitySplashBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    private val auth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+    private val SPLASH_DELAY = 1000L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val currentUser = auth.currentUser
+        Handler().postDelayed({
+            val intent = when {
+                currentUser != null && currentUser.isEmailVerified -> Intent(
+                    this,
+                    MainActivity::class.java
+                )
+
+                else -> Intent(this, LoginActivity::class.java)
+            }
+            startActivity(intent)
+            finish()
+        }, SPLASH_DELAY)
+    }
+}
