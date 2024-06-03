55cf87b960e7f485ec540cc57cf1fddda6f37ddb
Nguyen Ba Hieu <nbhieu140402@gmail.com>
2024-06-03 16:53:28 +0700

    update

--- app/src/main/java/com/example/qlsv/Account/ForgetPasswordActivity.kt
+++ app/src/main/java/com/example/qlsv/Account/ForgetPasswordActivity.kt
@@ -1,7 +1,11 @@
 package com.example.qlsv.Account
 
 import android.os.Bundle
+import android.os.Handler
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Patterns
+import android.view.View
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,21 +13,31 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.qlsv.R
 import com.example.qlsv.databinding.ActivityForgetPasswordBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class ForgetPasswordActivity : AppCompatActivity() {
     private lateinit var binding: ActivityForgetPasswordBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityForgetPasswordBinding.inflate(layoutInflater)
+        auth = FirebaseAuth.getInstance()
         setContentView(binding.root)
 
         clickEvents()
     }
 
+
     private fun clickEvents() {
         binding.txtLogin.setOnClickListener {
-            finish()
+            val handler = Handler()
+            binding.txtLogin.setTextColor(resources.getColor(R.color.blue))
+            handler.postDelayed({
+                binding.txtLogin.setTextColor(resources.getColor(R.color.black))
+                finish()
+            }, 100)
         }
+
         binding.btnConfirm.setOnClickListener {
             val inputEmail = binding.edtEmail.text.toString()
             when {
@@ -44,5 +58,106 @@ class ForgetPasswordActivity : AppCompatActivity() {
 
 
         }
+
+        binding.btnBack.setOnClickListener {
+            val handler = Handler()
+            binding.btnBack.setImageResource(R.drawable.icon_back_gray)
+            handler.postDelayed({
+                binding.btnBack.setImageResource(R.drawable.icon_back_white)
+                finish()
+            }, 100)
+
+        }
+
+        setupInputValidation()
+
+        binding.btnConfirm.setOnClickListener {
+            sendForgetPassword()
+        }
     }
+
+    private fun sendForgetPassword() {
+        val email = binding.edtEmail.text.toString().trim()
+        when {
+            email.isBlank() -> {
+                binding.txtErrorEmail.text = "Vui lòng nhập Email"
+                binding.txtErrorEmail.visibility = View.VISIBLE
+            }
+
+            !Patterns.EMAIL_ADDRESS.matcher(email).matches() -> {
+                binding.txtErrorEmail.text = "Vui lòng nhập Email đúng định dạng"
+                binding.txtErrorEmail.visibility = View.VISIBLE
+            }
+
+            else -> {
+                enableDisplay(false)
+                auth.sendPasswordResetEmail(email)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(
+                                this,
+                                "Vui lòng kiểm tra email để lấy lại mật khẩu",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            finish()
+                        } else {
+                            Toast.makeText(
+                                this,
+                                "Gửi Email đặt lại mật khẩu thất bại. Vui lòng kiểm tra lại Email",
+                                Toast.LENGTH_LONG
+                            ).show()
+                            enableDisplay(true)
+                        }
+                    }
+            }
+        }
+    }
+
+    private fun setupInputValidation() {
+        binding.edtEmail.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                binding.txtErrorEmail.visibility = View.INVISIBLE
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                val inputEmail = s.toString().trim()
+                when {
+                    inputEmail.isBlank() -> {
+                        binding.txtErrorEmail.text = "Email không được bỏ trống"
+                        binding.txtErrorEmail.visibility = View.VISIBLE
+                    }
+
+                    !Patterns.EMAIL_ADDRESS.matcher(inputEmail).matches() -> {
+                        binding.txtErrorEmail.text = "Vui lòng nhập Email đúng định dạng"
+                        binding.txtErrorEmail.visibility = View.VISIBLE
+                    }
+
+                    else -> {
+                        binding.txtErrorEmail.visibility = View.INVISIBLE
+                    }
+                }
+            }
+        })
+    }
+
+    private fun enableDisplay(status: Boolean) {
+        if (status) {
+            binding.btnBack.isEnabled = true
+            binding.edtEmail.isEnabled = true
+            binding.btnConfirm.isEnabled = true
+            binding.txtLogin.isEnabled = true
+            binding.btnConfirm.isEnabled = true
+            binding.layoutProgressBar.visibility = View.GONE
+        } else {
+            binding.btnBack.isEnabled = false
+            binding.edtEmail.isEnabled = false
+            binding.btnConfirm.isEnabled = false
+            binding.txtLogin.isEnabled = false
+            binding.btnConfirm.isEnabled = false
+            binding.layoutProgressBar.visibility = View.VISIBLE
+        }
+    }
+
 }
--- app/src/main/java/com/example/qlsv/Account/RegisterActivity.kt
+++ app/src/main/java/com/example/qlsv/Account/RegisterActivity.kt
@@ -1,45 +1,115 @@
 package com.example.qlsv.Account
 
+import android.content.Intent
+import android.content.res.Resources
 import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat
 import com.example.qlsv.R
 import com.example.qlsv.databinding.ActivityRegisterBinding
 import android.util.Patterns
 import android.widget.Toast
-import androidx.core.content.ContextCompat
-import java.util.regex.Pattern
+import com.example.qlsv.Class.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.firestore.FirebaseFirestore
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
-    private var STATUS_REGISTER_SUCCESS: Boolean = false
+    private lateinit var auth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
+
+    private val USER = "users"
+    private val DEFAULT_AVATAR =
+        "https://firebasestorage.googleapis.com/v0/b/qlsv-4e0cf.appspot.com/o/ImageDefault%2FAvatar.png?alt=media&token=f597b625-e043-4140-befd-db594e024472"
+    private val DEFAULT_COVER_IMAGE =
+        "https://firebasestorage.googleapis.com/v0/b/qlsv-4e0cf.appspot.com/o/ImageDefault%2FCoverImage.jpg?alt=media&token=1745252f-f1f1-481e-9702-d5ab6ca2b7fe"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
         clickEvents()
     }
 
     private fun clickEvents() {
         binding.txtLogin.setOnClickListener {
-            finish()
+            binding.txtLogin.setTextColor(resources.getColor(R.color.blue))
+            Handler().postDelayed({
+                binding.txtLogin.setTextColor(resources.getColor(R.color.blue_status))
+                finish()
+            }, 100)
+
         }
         binding.btnSignUp.setOnClickListener {
+
             if (validateInputs()) {
-                signUp()
+                enableDisplay(false)
+                val email = binding.edtEmail.text.toString().trim()
+                val password = binding.edtPassword.text.toString().trim()
+                val username = binding.edtUsername.text.toString().trim()
+                signUp(email, password, username)
+
             } else {
-                Toast.makeText(this, "Đăng ký không thành công", Toast.LENGTH_SHORT).show()
+                errorsRegister()
+            }
+
+        }
+
+        binding.btnBack.setOnClickListener {
+            val handler = Handler()
+            binding.btnBack.setImageResource(R.drawable.icon_back_gray)
+            handler.postDelayed({
+                binding.btnBack.setImageResource(R.drawable.icon_back_white)
+                finish()
+            }, 100)
+
+        }
+
+        setupInputValidation()
+    }
+
+    private fun errorsRegister() {
+        val username = binding.edtUsername.text.toString().trim()
+        val email = binding.edtEmail.text.toString().trim()
+        val password = binding.edtPassword.text.toString().trim()
+        val passwordConfirm = binding.edtPasswordConfirm.text.toString().trim()
+        val policy = binding.cbPolicy.isChecked
+        when {
+            username.isBlank() -> {
+                binding.txtErrorUsername.text = "Tên người dùng không được bỏ trống"
+                binding.txtErrorUsername.visibility = View.VISIBLE
+            }
+
+            email.isBlank() -> {
+                binding.txtErrorEmail.text = "Email không được bỏ trống"
+                binding.txtErrorEmail.visibility = View.VISIBLE
+            }
+
+            password.isBlank() -> {
+                binding.txtErrorPassword.text = "Mật khẩu không được bỏ trống"
+                binding.txtErrorPassword.visibility = View.VISIBLE
+            }
+
+            passwordConfirm.isBlank() -> {
+                binding.txtErrorPasswordConfirm.text = "Mật khẩu không được bỏ trống"
+                binding.txtErrorPasswordConfirm.visibility = View.VISIBLE
+            }
+
+            !policy -> {
+                binding.txtPolicy.setTextColor(Color.RED)
+                binding.txtAccept.setTextColor(Color.RED)
             }
         }
-        setupInputValidation()
-
     }
 
     private fun setupInputValidation() {
@@ -150,14 +220,6 @@ class RegisterActivity : AppCompatActivity() {
                 }
             }
         })
-    }
-
-    private fun validateInputs(): Boolean {
-        val isUsernameValid = binding.txtErrorUsername.visibility == View.INVISIBLE
-        val isEmailValid = binding.txtErrorEmail.visibility == View.INVISIBLE
-        val isPasswordValid = binding.txtErrorPassword.visibility == View.INVISIBLE
-        val isPasswordConfirmValid = binding.txtErrorPasswordConfirm.visibility == View.INVISIBLE
-        val isPolicyAccepted = binding.cbPolicy.isChecked
 
         binding.cbPolicy.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
@@ -168,24 +230,109 @@ class RegisterActivity : AppCompatActivity() {
                 binding.txtAccept.setTextColor(Color.RED)
             }
         }
-        binding.txtAccept.setTextColor(if (isPolicyAccepted) Color.BLACK else Color.RED)
-        binding.txtPolicy.setTextColor(
-            if (isPolicyAccepted) ContextCompat.getColor(
-                this,
-                R.color.blue_status
-            ) else Color.RED
-        )
-
-        return isUsernameValid && isEmailValid && isPasswordValid && isPasswordConfirmValid && isPolicyAccepted
     }
 
+    private fun validateInputs(): Boolean {
+        val isUsernameValid = binding.txtErrorUsername.visibility == View.INVISIBLE
+        val isEmailValid = binding.txtErrorEmail.visibility == View.INVISIBLE
+        val isPasswordValid = binding.txtErrorPassword.visibility == View.INVISIBLE
+        val isPasswordConfirmValid = binding.txtErrorPasswordConfirm.visibility == View.INVISIBLE
+        val isPolicyAccepted = binding.cbPolicy.isChecked
 
-    private fun signUp() {
-        Toast.makeText(this, "Đăng ký thành công", Toast.LENGTH_SHORT).show()
+        val inputUsername = binding.edtUsername.text.toString().trim()
+        val inputEmail = binding.edtEmail.text.toString().trim()
+        val inputPassword = binding.edtPassword.text.toString().trim()
+        val inputPasswordConfirm = binding.edtPasswordConfirm.text.toString().trim()
+        if (inputUsername.isBlank() && inputEmail.isBlank() && inputPassword.isBlank() && inputPasswordConfirm.isBlank()) {
+            return isUsernameValid && isEmailValid && isPasswordValid && isPasswordConfirmValid && isPolicyAccepted
+        }
+        return false
+    }
+
+    private fun signUp(email: String, password: String, username: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { result ->
+                if (result.isSuccessful) {
+                    val user = auth.currentUser
+                    user?.let {
+                        val userID = it.uid
+                        val newUser = User(
+                            userID,
+                            email,
+                            password,
+                            username,
+                            DEFAULT_AVATAR,
+                            DEFAULT_COVER_IMAGE
+                        )
+                        firestore.collection(USER).document(userID).set(newUser)
+                            .addOnSuccessListener {
+                                Log.d("Register", "verify")
+                                verifyEmail()
+                            }
+                            .addOnFailureListener { exception ->
+                                Log.d("Register", "Error Failure: ${exception.message}")
+                                Toast.makeText(this, "Đăng ký không thành công", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                    }
+                    enableDisplay(true)
+                } else {
+                    emailAlreadyExists(result.exception)
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("Register", "Error Failure: ${exception.message}")
+                Toast.makeText(this, "Đăng ký không thành công", Toast.LENGTH_SHORT).show()
+            }
+
+    }
+
+    private fun verifyEmail() {
+        auth.currentUser
+            ?.sendEmailVerification()
+            ?.addOnSuccessListener {
+                Toast.makeText(this, "Vui lòng kiểm tra email để xác thực", Toast.LENGTH_SHORT)
+                    .show()
+                finish()
+            }
+            ?.addOnFailureListener {
+                Toast.makeText(this, "Gửi xác thực email thất bại", Toast.LENGTH_SHORT).show()
+            }
     }
 
     fun hasSpecialCharacters(string: String): Boolean {
         val regex = Regex("[^\\w\\s]")
         return regex.containsMatchIn(string)
+    }
+
+    private fun emailAlreadyExists(e: Exception?) {
+        if (e is FirebaseAuthUserCollisionException) {
+            Toast.makeText(this, "Email đã tồn tại", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Đăng ký thất bại. Vui lòng thử lại.", Toast.LENGTH_SHORT).show()
+        }
+        enableDisplay(true)
+    }
+
+    private fun enableDisplay(status: Boolean) {
+        if (status) {
+            binding.edtUsername.isEnabled = true
+            binding.edtEmail.isEnabled = true
+            binding.edtPassword.isEnabled = true
+            binding.edtPasswordConfirm.isEnabled = true
+            binding.cbPolicy.isEnabled = true
+            binding.btnSignUp.isEnabled = true
+            binding.layoutProgressBar.visibility = View.GONE
+            binding.txtLogin.isEnabled = true
+        } else {
+            binding.edtUsername.isEnabled = false
+            binding.edtEmail.isEnabled = false
+            binding.edtPassword.isEnabled = false
+            binding.edtPasswordConfirm.isEnabled = false
+            binding.cbPolicy.isEnabled = false
+            binding.btnSignUp.isEnabled = false
+            binding.layoutProgressBar.visibility = View.VISIBLE
+            binding.txtLogin.isEnabled = false
+        }
     }
 }
